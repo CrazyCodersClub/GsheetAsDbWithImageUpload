a552643fa00036f8e292047613d85c8773468254
androidLabsCode <mr.arundj@gmail.com>
2017-08-10 00:20:56 +0530

    Update Configuration.java

--- app/src/main/java/alabs/gsheetwithimage/Configuration.java
+++ app/src/main/java/alabs/gsheetwithimage/Configuration.java
@@ -6,9 +6,9 @@ package alabs.gsheetwithimage;
 
 public class Configuration {
 
-
-    public static final String ADD_USER_URL = "https://script.google.com/macros/s/AKfycbwV8T943LCouWPV4Owds4XeTRmLTzocBZNE2LBoRG5b0BwWPJQ/exec";
-    public static final String LIST_USER_URL = "https://script.google.com/macros/s/AKfycbwV8T943LCouWPV4Owds4XeTRmLTzocBZNE2LBoRG5b0BwWPJQ/exec?action=readAll";
+    public static final String APP_SCRIPT_WEB_APP_URL = "ADD YOUR APP SCRIPT WEB APP URL";
+    public static final String ADD_USER_URL = APP_SCRIPT_WEB_APP_URL;
+    public static final String LIST_USER_URL = APP_SCRIPT_WEB_APP_URL+"?action=readAll";
 
     public static final String KEY_ID = "uId";
     public static final String KEY_NAME = "uName";
